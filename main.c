2f147f6277d21ba191cb6ff8c62a6e5a0bb369a8
Andrea Pérez <48370962+AndreaPerezAr@users.noreply.github.com>
2022-07-21 16:21:15 -0500

    Init commit

--- main.c
+++ main.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
 #include <assert.h>
-#include <colorpairs.h>
+
+#define TOTAL_PAIRS 25
+
+//Location of variables
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
+
+//Location of functions
+void ColorPairToString(const ColorPair* colorPair, char* buffer) {
+    sprintf(buffer, "%s %s",
+        MajorColorNames[colorPair->majorColor],
+        MinorColorNames[colorPair->minorColor]);
+}
+
+ColorPair GetColorFromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    int zeroBasedPairNumber = pairNumber - 1;
+    colorPair.majorColor = 
+        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor =
+        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return colorPair;
+}
+
+int GetPairNumberFromColor(const ColorPair* colorPair) {
+    return colorPair->majorColor * numberOfMinorColors +
+            colorPair->minorColor + 1;
+}
 
 void testNumberToPair(int pairNumber,
     enum MajorColor expectedMajor,
@@ -30,9 +68,7 @@ void testPairToNumber(
 void ReferenceManual()
 {
     int pair_number;
-    int total_pairs=25;
-
-    for(pair_number=1;pair_number<=total_pairs;pair_number++)
+    for(pair_number=1;pair_number<=TOTAL_PAIRS;pair_number++)
     {
         ColorPair colorPair = GetColorFromPairNumber(int pair_number);
         printf("%n    ", pair_number);
